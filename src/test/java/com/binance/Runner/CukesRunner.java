02fded7698332824acf306f3b0f32d2ee8bee14a
wcc-2 <bayrambarlasbayram@gmail.com>
2021-02-01 15:52:02 +0200

    Tags updated!

--- src/test/java/com/binance/Runner/CukesRunner.java
+++ src/test/java/com/binance/Runner/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/",
         glue = "com/binance/StepDefinitions/",
         dryRun = false,
-        tags = " @ALG"
+        tags = "@Gainers"
 )
 public class CukesRunner {
 }
